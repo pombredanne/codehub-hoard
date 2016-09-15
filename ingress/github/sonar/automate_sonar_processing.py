3789aec20bdd37bce77c17d7cff2c75ed284c38c
Nathaniel Solomon <ddeemeks@gmail.com>
2016-09-15 15:14:31 -0700

    #10 breaking and refactoring the sonar script to remove dependencies for multiple run

--- ingress/github/sonar/automate_sonar_processing.py
+++ ingress/github/sonar/automate_sonar_processing.py
@@ -10,6 +10,7 @@ import time
 import logging
 from subprocess import call,check_output, run
 import subprocess
+
 def collect_repositries(config):
     logging.info(time.strftime("%c")+' collecting repositories name, clone_url')
     configurations = config['config']
@@ -30,6 +31,7 @@ def get_public_repos(config):
     orgs = configurations['public_orgs']
     for org in orgs:
         res = requests.get(configurations['public_github_api_url']+'/orgs/' + org + '/repos?access_token='+access_token,verify=False)
+        print(configurations['public_github_api_url']+'/orgs/' + org + '/repos?access_token='+access_token)
         orgs_public_repositories = orgs_public_repositories + json.loads(res.text)
     return orgs_public_repositories
 
@@ -209,87 +211,11 @@ def adjust_language_naming(repo_language):
             lang = repo_language.lower()
     return lang
 
-def get_es_project(config,results):
-    returned_responses_collection = []
-    configurations = config['config']
-    for res in results:
-        query_data = {
-                      "query": {
-                        "bool": {
-                          "must": [
-                            { "match": { "project_name":  res['project_name'] }},
-                            { "match": { "organization": res['org']}}
-                          ]
-                        }
-                      }
-                    }
-        response = requests.get(configurations['stage_es_url']+'/projects/logs/_search', data=json.dumps(query_data))
-        returned_res = json.loads(response.text)
-        collected_response = {}
-        if returned_res['hits']['total'] > 0:
-            collected_response['metrics'] = res['metrics']
-            collected_response['returned_res'] = returned_res
-            returned_responses_collection.append(collected_response)
-    return returned_responses_collection
-
-def makeEsUpdates(config,returned_responses):
-    collected_ids = []
-    collected_response = []
-    configurations = config['config']
-    for ret in returned_responses:
-        project_id = ret['returned_res']['hits']['hits'][0]['_id']
-        filtered_health_metrics = ret['metrics']
-
-        if len(filtered_health_metrics) > 0:
-            data_json = {"project_health_metrics": filtered_health_metrics}
-            update_query = {
-              "doc": data_json
-            }
-            ret_response = requests.post(configurations['stage_es_url']+'/projects/logs/'+project_id+'/_update', data=json.dumps(update_query))
-            if project_id not in collected_ids:
-                collected_ids.append(project_id)
-                collected_response.append(json.loads(ret_response.text))
-    return collected_response
-
-
-def process_elasticSearch_update(results, config):
-    if config['update'] == 'results.out':
-        logging.info(time.strftime("%c")+' Writing the result data to a local file results.out')
-        write_results(config['update'], results)
-    else:
-        logging.info(time.strftime("%c")+' talking to ES and adding project depedency attribute with processed data')
-        res_arr = get_es_project(config,results)
-        collected_response = makeEsUpdates(config,res_arr)
-        display_stats(config, collected_response)
-
-def write_results(resultfile,results):
-    file_object = open(os.getcwd()+"/"+resultfile, 'w')
-    file_object.write("\t******************** The following Projects have dependencies ********************************\n\n")
-    for res in results:
-        file_object.write(json.dumps(res))
-        file_object.write("\n")
-
-def display_stats(config, response_arr):
-    logging.info(time.strftime("%c")+" ******The following "+str(len(response_arr)) + " projects have health metrics ******")
-    repos_just_updated = []
-    configurations = config['config']
-    for repo in response_arr:
-        response = requests.get(configurations['stage_es_url']+'/projects/logs/'+repo['_id'])
-        logging.info(json.loads(response.text))
-        if(repo['_shards']['successful'] == 1):
-            repos_just_updated.append(json.loads(response.text))
-
-    if(len(repos_just_updated) > 0):
-        logging.info("******The following projects are "+ str(len(repos_just_updated))+" just updated******")
-        for repo in repos_just_updated:
-            logging.info(repo)
-    else:
-        logging.info(time.strftime("%c")+" **** No Project is updated ****")
-
-def cleanup_after_update():
-    clone_dir = os.getcwd() + '/cloned_projects/'
-    logging.info(time.strftime("%c")+' cleaning up cloned projects after elasticsearch updates or written to a file')
-    delete_directory(clone_dir)
+def create_result_json(filtered_repo, result_path):
+    file_object = open(result_path, 'w')
+    with open(result_path, 'w') as outfile:
+        json.dump(filtered_repo, outfile, indent=4, sort_keys=True, separators=(',', ':'))
+    return result_path
 
 def automate_processes(config):
     sonar_dir = os.getcwd()+"/**/sonar-runner"
@@ -297,10 +223,11 @@ def automate_processes(config):
     clone_projects(repos,config)
     processed_repos = process_cloned_projects(repos)
     build_sonar_project_config(processed_repos,config)
-
     filtered_repo = make_sonar_api_call(processed_repos,config)
-    process_elasticSearch_update(filtered_repo, config)
-    #cleanup_after_update()
+    os.chdir("..")
+    res_dir = os.getcwd()+"/metric_result.json"
+    create_result_json(filtered_repo,res_dir)
+
 
 if __name__ == "__main__":
     parsed = configparams._parse_commandline()
