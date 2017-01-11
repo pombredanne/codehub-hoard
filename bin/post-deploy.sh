975912b513e3e130b67bdbc41fee2ef64cbb4fd5
Barakoti Thapliya <544088@bah.com>
2017-01-10 22:21:27 -0500

    added prod config

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -4,6 +4,6 @@ INGEST_TOOLS=${ingest.tools.dir}
 
 echo Starting Nifi ...
 #$INGEST_TOOLS/nifi-*/bin/nifi.sh start
-#sudo -u ec2-user $INGEST_TOOLS/nifi-*/bin/nifi.sh start
+#sudo -u ec2-user $INGEST_TOOLS/nifi-*/bin/nifi.sh start > /dev/null 2>&1 &
 
 exit 0
