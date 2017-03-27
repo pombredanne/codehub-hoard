343447c0d427f07f1b0caf3165370a3833788e83
Barakoti Thapliya <544088@bah.com>
2017-03-27 10:08:36 -0400

    added vcd prog properties

--- bin/pre-deploy.sh
+++ bin/pre-deploy.sh
@@ -2,12 +2,12 @@
 
 INGEST_TOOLS=${ingest.tools.dir}
 INGEST_HOME=${ingest.home}
+DATA_HOME=${ingest.data.home.dir}
 
 #Save old config file settings before deploying. This will be removed once we
 #have a mechanism to encrypt the github token in config files
-cp $INGEST_HOME/config/application.conf $INGEST_HOME/config/application_orig.conf
-cp $INGEST_HOME/ingress/github/code/ingest_orig.conf $INGEST_HOME/ingress/github/code/ingest.conf
-cp $INGEST_HOME/ingress/github/sonar/ingest_orig.conf $INGEST_HOME/ingress/github/sonar/ingest.conf
+cp $INGEST_HOME/config/application.conf $DATA_HOME/config/application_orig.conf
+cp $INGEST_HOME/ingress/github/code/ingest.conf $DATA_HOME/ingress/github/code/ingest_orig.conf
 
 
 #Experimental for now
