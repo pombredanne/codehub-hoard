ff92e9d18ecab9a5feed7e03c042c093da8f4bf5
Gio <rodriguez_carlos@bah.com>
2019-07-02 17:17:07 -0400

    #ODE-1316 : Wrapping code in a Class ExecVirusScan

--- ingress/github/vscan/exec_virus_scan.py
+++ ingress/github/vscan/exec_virus_scan.py
@@ -19,125 +19,128 @@ sys.path.append(os.path.abspath("../config"))
 import configparams
 ssl_verify='/etc/ssl/cert.pem'
 
-def _process_metric_line(line):
-    if line is None:
-        return None, None
+class ExecVirusScan():
 
-    supportedMetrics = ["Scanned directories","Scanned files","Infected files","Data scanned","Time"]
+    def _process_metric_line(self, line):
+        if line is None:
+            return None, None
 
-    parts = line.split(':')
-    if len(parts) != 2:
-        return None, None
+        supportedMetrics = ["Scanned directories","Scanned files","Infected files","Data scanned","Time"]
 
-    name = parts[0].strip()
-    if name.lower() not in (x.lower() for x in supportedMetrics):
-        return None, None
+        parts = line.split(':')
+        if len(parts) != 2:
+            return None, None
 
-    name = name.lower().replace(" ","_")
-    val = parts[1].strip()
-    if val.isdigit():
-        v = int(val)
-        val = v
+        name = parts[0].strip()
+        if name.lower() not in (x.lower() for x in supportedMetrics):
+            return None, None
 
-    return name, val
+        name = name.lower().replace(" ","_")
+        val = parts[1].strip()
+        if val.isdigit():
+            v = int(val)
+            val = v
 
-def _process_file_line(line, ref_path):
-    if line is None:
-        return None
+        return name, val
 
-    parts = line.split(" ")
-    if len(parts) != 3:
-        return None
+    def _process_file_line(self, line, ref_path):
+        if line is None:
+            return None
 
-    result = {}
-    file_name = parts[0][:-1]
-    p = file_name.find(ref_path)
-    if p >= 0:
-        file_name = file_name[p:]
+        parts = line.split(" ")
+        if len(parts) != 3:
+            return None
 
-    result['filename'] = file_name
-    result['virus'] = parts[1]
-    return result
+        result = {}
+        file_name = parts[0][:-1]
+        p = file_name.find(ref_path)
+        if p >= 0:
+            file_name = file_name[p:]
+
+        result['filename'] = file_name
+        result['virus'] = parts[1]
+        return result
 
 
-def _process_vscan_output(output, ref_path):
-    if output is None:
-        return None
+    def _process_vscan_output(self, output, ref_path):
+        if output is None:
+            return None
 
-    lines = output.splitlines()
-    if len(lines) <= 0:
-        return None
+        lines = output.splitlines()
+        if len(lines) <= 0:
+            return None
 
-    result = {}
-    files = []
-    metrics = False
-    for line in lines:
-        if not metrics:
-            if "FOUND" in line:
-                pline = _process_file_line(line, ref_path)
-                if pline is None:
+        result = {}
+        files = []
+        metrics = False
+        for line in lines:
+            if not metrics:
+                if "FOUND" in line:
+                    pline = self._process_file_line(line, ref_path)
+                    if pline is None:
+                        continue
+                    files.append(pline)
+                if "SCAN SUMMARY" in line:
+                    metrics = True
                     continue
-                files.append(pline)
-            if "SCAN SUMMARY" in line:
-                metrics = True
-                continue
-        else:
-            mName, mValue = _process_metric_line(line)
-            if mName is None:
-                continue
-            result[mName] = mValue
-    result['lastscan'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    result['reported_files'] = files
-    return result
+            else:
+                mName, mValue = self._process_metric_line(line)
+                if mName is None:
+                    continue
+                result[mName] = mValue
+        result['lastscan'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        result['reported_files'] = files
+        return result
 
 
-def _execute_vscan(cloned_project_path, ref_path):
-    target = cloned_project_path
-    proc = Popen(['clamscan', '-i', '-o', '-r', target], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = proc.communicate()
-    rc = proc.returncode
-    # rc values: 0=no virus found, 1=virus found, 2=error
-    if rc == 2:
-        print("ERROR: " + err)
-        return None
+    def _execute_vscan(self, cloned_project_path, ref_path):
+        target = cloned_project_path
+        proc = Popen(['clamscan', '-i', '-o', '-r', target], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = proc.communicate()
+        rc = proc.returncode
+        # rc values: 0=no virus found, 1=virus found, 2=error
+        if rc == 2:
+            print("ERROR: " + err)
+            return None
 
-    report = _process_vscan_output(output, ref_path)
-    return report
+        report = self._process_vscan_output(output, ref_path)
+        return report
 
 
-def _process_messages(config, messages):
-    for message in messages:
-        if message is not None:
-            data = pickle.loads(message.value)
-            if data is None:
-              continue
-            print(time.strftime("%c")+" Scanning files for: "+data['org']+"->"+data['project_name'])
-            cloned_project_path = data["cloned_project_path"]
-            if not os.path.isdir(cloned_project_path):
-                print("ERROR: Path not found: "+ cloned_project_path)
+    def _process_messages(self, config, messages):
+        for message in messages:
+            if message is not None:
+                data = pickle.loads(message.value)
+                if data is None:
+                    continue
+                print(time.strftime("%c")+" Scanning files for: "+data['org']+"->"+data['project_name'])
+                cloned_project_path = data["cloned_project_path"]
+                if not os.path.isdir(cloned_project_path):
+                    print("ERROR: Path not found: "+ cloned_project_path)
+                    print(time.strftime("%c")+" Scan completed.")
+                    continue
+                ref_path = data["org"]+"/"+data["project_name"]
+                vscan_result = self._execute_vscan(cloned_project_path, ref_path)
+                if vscan_result is None:
+                    print("WARNING: No results")
+                    print(time.strftime("%c")+" Scan completed.")
+                    continue
+                data["vscan"] = vscan_result
+                print('MSG: VIRUS_SCAN_QUEUE')
+                kafkaProducer.publish_kafka_message(data,config,'VIRUS_SCAN_QUEUE')
                 print(time.strftime("%c")+" Scan completed.")
-                continue
-            ref_path = data["org"]+"/"+data["project_name"]
-            vscan_result = _execute_vscan(cloned_project_path, ref_path)
-            if vscan_result is None:
-                print("WARNING: No results")
-                print(time.strftime("%c")+" Scan completed.")
-                continue
-            data["vscan"] = vscan_result
-            print('MSG: VIRUS_SCAN_QUEUE')
-            kafkaProducer.publish_kafka_message(data,config,'VIRUS_SCAN_QUEUE')
-            print(time.strftime("%c")+" Scan completed.")
 
 
-def automate_processes(config):
-    print(time.strftime("%c")+" VScan processing started")
-    messages = kafkaConsumer._get_balanced_consumer(config,'consumer_vscan_group')
-    _process_messages(config, messages)
-    print(time.strftime("%c")+" VScan processing started")
+    def automate_processes(self, config):
+        print(time.strftime("%c")+" VScan processing started")
+        messages = kafkaConsumer._get_balanced_consumer(config,'consumer_vscan_group')
+        self._process_messages(config, messages)
+        print(time.strftime("%c")+" VScan processing started")
 
 
 if __name__ == "__main__":
     parsed = configparams._parse_commandline()
     config = configparams.main(parsed)
-    automate_processes(config)
+    evs = ExecVirusScan()
+    evs.automate_processes(config)
 
